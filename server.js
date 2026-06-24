7fba66f9096e31c42af72baabfdd25469366a55d
Nikhil Wankhade <nikhilw395@gmail.com>
2026-06-24 17:53:56 +0530

    fix: add .env in gitignore ; remoed extra vars

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import app from './src/app.js';
-import connectDB from './src/config/Db.js';
+import app from "./src/app.js";
+import connectDB from "./src/config/Db.js";
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server is running at port ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Server startup failed:', error);
-        process.exit(1);
-    }
+  try {
+    await connectDB();
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server startup failed:", error);
+    process.exit(1);
+  }
 }
 
 startServer();
--- src/config/Db.js
+++ src/config/Db.js
@@ -1,24 +1,20 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import dns from 'dns';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import dns from "dns";
 
 dotenv.config();
 
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-const MONGO_URI = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb+srv://YtttT:7ajZzyytJmeEDlLc@yt-complete-backend.s5v1w2j.mongodb.net/Products?retryWrites=true&w=majority';
-
+const MONGO_URI = process.env.MONGO_URI;
 async function connectDB() {
-    try {
-        await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('DB connected successfully');
-    } catch (error) {
-        console.error('DB connection error:', error.message);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(MONGO_URI);
+    console.log("DB connected successfully " + conn.connection.host);
+  } catch (error) {
+    console.error("DB connection error:", error.message);
+    process.exit(1);
+  }
 }
 
 export default connectDB;
