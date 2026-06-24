fa09b36a07d8317958974ce8be34deb8961d83f4
usergreats2567-maker <usergreats2567@gmail.com>
2026-06-24 17:10:45 +0530

    Use env MongoDB URI in Db config

--- src/config/Db.js
+++ src/config/Db.js
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose';
-// import dns from 'dns';
-import dns from "dns";
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
+import dotenv from 'dotenv';
+import dns from 'dns';
 
-// 'mongodb+srv://YtttT:7ajZzyytJmeEDlLc@yt-complete-backend.s5v1w2j.mongodb.net/Products?retryWrites=true&w=majority'
+dotenv.config();
 
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 
+const MONGO_URI = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb+srv://YtttT:7ajZzyytJmeEDlLc@yt-complete-backend.s5v1w2j.mongodb.net/Products?retryWrites=true&w=majority';
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://YtttT:7ajZzyytJmeEDlLc@yt-complete-backend.s5v1w2j.mongodb.net/Products?retryWrites=true&w=majority', {
+        await mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
